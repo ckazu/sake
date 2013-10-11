ad69cf9b5b2a6aa5a9b6f1aca3be2248e7e8ecb1
CHINDA, Kazuyuki <ckazu.s@gmail.com>
2013-10-11 21:30:17 +0900

    Add inherited_resources templates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def linkable?(name)
+    %w(id title name).include? name
+  end
+
+  def date?(obj)
+    obj.is_a? Date
+  end
+
+  def time?(obj)
+    obj.is_a? Time
+  end
+
+  def date_or_time?(obj)
+    date?(obj) or time?(obj)
+  end
+  alias_method :time_or_date?, :date_or_time?
+
+  def relational?(name)
+    respond_to?(:parent) && name == "#{parent.class.name.downcase}_#{parent.class.primary_key}"
+  end
+
+  def relational_title
+    parent.try(:name) || parent.try(:title) || parent.try(:description) || parent.id
+  end
 end
