cdfa1056e44c003e5a22e3e655c3d57bb1f6b60e
CHINDA, Kazuyuki <ckazu.s@gmail.com>
2013-10-11 21:32:12 +0900

    Generate article CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
-class ArticlesController < ApplicationController
+class ArticlesController < InheritedResources::Base
+  private
+  def permitted_params
+    params.permit article: %w|title body|
+  end
 end
