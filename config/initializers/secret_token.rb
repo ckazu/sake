97fa2e311adb1ca331b973fe20e6664e444dd5ea
CHINDA, Kazuyuki <ckazu.s@gmail.com>
2013-10-11 20:33:37 +0900

    rails new sake --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sake::Application.config.secret_key_base = '80ca15e0dcd1aa111e044e48e6aff4ecdd75f8c6fc1d87a1ff326167a92f100484b57e1a2caefe463fd27246471530c849e95d9b88d0b5853aa2dc09653556b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sake::Application.config.session_store :cookie_store, key: '_sake_session'
